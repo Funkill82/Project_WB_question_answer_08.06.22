fd73fd10e52b89d18fbeb8a8638d4811176ef340
funkill82 <funkill@ukr.net>
2022-06-08 17:32:57 +0300

    first commit

---
+++ WB_question_answe.py
@@ -0,0 +1,45 @@
+import requests
+import xlsxwriter
+
+
+def unloading_excel(user, country, question, answer):
+    """ Выгружаю данные в файл excel """
+    try:
+        workbook = xlsxwriter.Workbook('data.xlsx')
+        worksheet = workbook.add_worksheet()
+        worksheet.write(0, 0, 'Имя пользователя')
+        worksheet.write(0, 1, 'Страна')
+        worksheet.write(0, 2, 'Вопрос пользователя')
+        worksheet.write(0, 3, 'Ответ')
+        for i in range(1, len(user)):
+            worksheet.write(i, 0, user[i])
+            worksheet.write(i, 1, country[i])
+            worksheet.write(i, 2, question[i])
+            worksheet.write(i, 3, answer[i])
+    except Exception as e:
+        print('Ошибка создания файла')
+
+    finally:
+        workbook.close()
+
+def parse(data):
+    """ Вытаскиваю данные из json """
+    name_user, user_country, question_text, answer_text = [], [], [], []
+
+    for item in data['questions']:
+        name_user.append(item['wbUserDetails']['name'])
+        user_country.append(item['wbUserDetails']['country'])
+        question_text.append(item['text'].replace('\n', ' '))
+        answer_text.append(item['answer']['text'].replace('\n', ''))
+
+    unloading_excel(name_user, user_country, question_text, answer_text)
+
+
+URL = "https://questions.wildberries.ru/api/v1/questions?imtId=30499672&skip=0&take=20"
+
+r = requests.get(url=URL)
+
+data = r.json()
+
+parse(data)
+
